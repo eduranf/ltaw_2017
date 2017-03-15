4af44c4102ca4eabe1a77928823e3114519582cb
Eduardo Durán Fuentes <eduardoduranfuentes@gmail.com>
2017-03-15 13:08:20 +0100

    actualizando las vistas, usando plantillas

--- p4/mysite/polls/models.py
+++ p4/mysite/polls/models.py
@@ -24,3 +24,19 @@ class Choice(models.Model):
         return self.choice_text
 
 # Create your models here.
+
+
+#DUDAS:
+    #1)Si creo una instancia de un modelo (por ejemplo, Question) y la guardo en una variable "q" en la shell de python, y luego la guardo en la base de datos,
+    #se le asigna una "id". Ejemplo--> id =1.
+    #2)Supongamos que ahora quiero borrar la variable "q" de la base de datos. No se restaura la id de q.
+    #3)Ahora creo una nueva instancia del modelo "Question", lo guardo en una variable, lo guardo en la base de datos y se le asigna una id. En este caso,id =  2.No entiendo.
+
+    #Crear una instancia del modelo Question.
+        #// q = Question(question_text="What's new?", pub_date=timezone.now())///
+
+    # Guardar la instancia de "Question" en la base de datos
+        #q.save()
+
+    #Eliminar una entrada en la base de datos
+        #q.delete()
